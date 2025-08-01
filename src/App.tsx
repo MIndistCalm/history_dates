d88ae0ba4edd292dad604776ec6c2fac22bb1dc8
MindistCalm <suxinin5@yandex.ru>
2025-08-02 03:10:40 +0700

    3 | added circle animation and logic, mock data, types, constants, colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { HistoryDates } from './components'
-
+import './App.scss'
 export const App = () => {
   return (
-    <main>
-      <HistoryDates />
+    <main className='main-container'>
+      <div className='col-start-3-span-9'>
+        <HistoryDates />
+      </div>
     </main>
   )
 }
